3e5cdf3842a32951bbdd472081223ab3222e67c6
JohnAnon9771 <njoao97710@gmail.com>
2020-05-19 15:09:23 -0300

    added styling for component carousel

--- App.tsx
+++ App.tsx
@@ -1,35 +1,7 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Animated from "react-native-reanimated";
-import { spring } from "react-native-redash";
+
+import Carousel from "./src/components/Carousel";
 
 export default function App() {
-  const { Value, useCode, set } = Animated;
-  const y = new Value(0);
-  useCode(
-    () => set(y, spring({ to: y, from: 50, config: { damping: 10 } })),
-    []
-  );
-  return (
-    <View style={styles.container}>
-      <Animated.View style={{ transform: { translateY: y } }}>
-        <View style={styles.ball} />
-      </Animated.View>
-    </View>
-  );
+  return <Carousel />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  ball: {
-    backgroundColor: "red",
-    borderRadius: 50,
-    height: 50,
-    width: 50,
-  },
-});
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { Dimensions } from "react-native";
+import Animated from "react-native-reanimated";
+
+const { width: screenWidth } = Dimensions.get("window");
+
+const CARD_PADDING = 50;
+const CARD_WIDTH = screenWidth - CARD_PADDING * 2;
 
 export default function Carousel() {
-  return null;
+  const { ScrollView, View } = Animated;
+  return (
+    <ScrollView
+      horizontal
+      snapToAlignment="start"
+      decelerationRate="fast"
+      snapToInterval={CARD_WIDTH}
+      style={{ paddingVertical: 50 }}
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingHorizontal: CARD_PADDING }}
+    >
+      {Array.from({ length: 10 }).map((card, index) => (
+        <View
+          key={index}
+          style={{
+            flex: 1,
+            width: CARD_WIDTH,
+            borderWidth: 1,
+            borderColor: "red",
+            backgroundColor: "lightgreen",
+          }}
+        />
+      ))}
+    </ScrollView>
+  );
 }
