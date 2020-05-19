f5c783e485aaa7f70cc45b0da9c65348a1aa42f6
JohnAnon9771 <njoao97710@gmail.com>
2020-05-19 15:00:16 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Animated from "react-native-reanimated";
+import { spring } from "react-native-redash";
 
 export default function App() {
+  const { Value, useCode, set } = Animated;
+  const y = new Value(0);
+  useCode(
+    () => set(y, spring({ to: y, from: 50, config: { damping: 10 } })),
+    []
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Animated.View style={{ transform: { translateY: y } }}>
+        <View style={styles.ball} />
+      </Animated.View>
     </View>
   );
 }
@@ -12,8 +22,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  ball: {
+    backgroundColor: "red",
+    borderRadius: 50,
+    height: 50,
+    width: 50,
   },
 });
