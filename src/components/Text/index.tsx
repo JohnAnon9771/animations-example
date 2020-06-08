82fb8ab3b6f31b335e0ccf81cb695f6d38803fca
JohnAnon9771 <njoao97710@gmail.com>
2020-06-08 00:44:51 -0300

    fix component text, import not necessary

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,33 +1,15 @@
 import React from "react";
-import { Text, FontVariant, StyleProp, TextStyle } from "react-native";
+import { Text } from "react-native";
 import { AppLoading } from "expo";
 import { useFonts } from "@use-expo/font";
 
 interface Props {
   size?: number;
-  fontWeight?:
-    | "normal"
-    | "bold"
-    | "100"
-    | "200"
-    | "300"
-    | "400"
-    | "500"
-    | "600"
-    | "700"
-    | "800"
-    | "900";
   color?: string;
   font?: "Gilroy-ExtraBold" | "Gilroy-Light";
 }
 
-const TextComponent: React.FC<Props> = ({
-  children,
-  size,
-  color,
-  fontWeight,
-  font,
-}) => {
+const TextComponent: React.FC<Props> = ({ children, size, color, font }) => {
   const [isLoaded] = useFonts({
     "Gilroy-ExtraBold": require("../../assets/fonts/Gilroy-ExtraBold.otf"),
     "Gilroy-Light": require("../../assets/fonts/Gilroy-Light.otf"),
@@ -41,7 +23,6 @@ const TextComponent: React.FC<Props> = ({
         style={{
           fontFamily: !font ? "Gilroy-Light" : font,
           fontSize: size,
-          fontWeight,
           color,
         }}
       >
