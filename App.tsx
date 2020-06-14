b38af7643571260788f713acbf1b5ef85b0b90bf
JohnAnon9771 <njoao97710@gmail.com>
2020-06-14 20:18:31 -0300

    initial new animation

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
 import React from "react";
-import { StatusBar } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import Routes from "./src/navigation";
+import { View } from "react-native";
 
 export default function App() {
-  return (
-    <>
-      <StatusBar
-        translucent
-        backgroundColor="transparent"
-        barStyle="dark-content"
-      />
-      <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
-    </>
-  );
+  return <View></View>;
 }
